e964393e21d03f195529306ec89d29242643b0ed
penglin <987532488@qq.com>
2019-05-16 19:24:24 +0800

    add exercise 21

--- Exercises.py
+++ Exercises.py
@@ -292,9 +292,17 @@ def exercise20() -> None:
         total += 2*100/(2**i)
     print(total)
 
+# exercise 21
+def exercise21(n:int) -> int:
+    """猴子吃桃问题：猴子第一天摘下若干个桃子，当即吃了一半，还不瘾，又多吃了一个第二天早上又将剩下的桃子吃掉一半，
+    又多吃了一个。以后每天早上都吃了前一天剩下的一半零一个。到第10天早上想再吃时，见只剩下一个桃子了。求第一天共摘了多少。"""
+    if n == 1:
+        return 1
+    return 2*(exercise21(n-1)+1)
+
 if __name__ == "__main__":
     #print(exercise6(100))
     # print(exercise11(11))
     # print(PrimeNumber(100))
     # print(exercise14(28))
-    exercise20()
+    print(exercise21(10))
