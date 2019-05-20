775984cc899d76600b058b4a895b6d35006ad428
penglin <987532488@qq.com>
2019-05-21 00:21:02 +0800

    add .gitignore and CIFAR10.py

--- CIFAR10.py
+++ CIFAR10.py
@@ -1,18 +1,96 @@
 import torch
 import torchvision
+import matplotlib.pyplot as plt
+import numpy as np
 import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
-transform = transforms.Compose(
+def dataset():
+    transform = transforms.Compose(
     [transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root="Q:\LEARN\pythonExercises",train=True,
-    download=True,transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset,batch_size=4,shuffle=True,num_workers=2)
+    trainset = torchvision.datasets.CIFAR10(root="./",train=True,
+    download=False,transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset,batch_size=4,shuffle=True,num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root="Q:\LEARN\pythonExercises",train=False,
-    download=True,transform=transform)
-testloader = torch.utils.data.DataLoader(testset,batch_size=4,shuffle=False,num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root="./",train=False,
+    download=False,transform=transform)
+    testloader = torch.utils.data.DataLoader(testset,batch_size=4,shuffle=False,num_workers=2)
+    return trainloader,testloader
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def imshow(img):
+    img = img /2 +0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg,(1,2,0)))
+
+class Net(nn.Module):
+    """网络"""
+    def __init__(self):
+        super(Net,self).__init__()
+        self.conv1 = nn.Conv2d(3,6,5)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(6,16,5)
+        self.fc1 = nn.Linear(16*5*5,120)
+        self.fc2 = nn.Linear(120,84)
+        self.fc3 = nn.Linear(84,10)
+
+    def forward(self,x) -> torch.Tensor:
+        """forward"""
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1,16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+if __name__ == "__main__":
+    trainloader, testloader = dataset()
+    # classes = ('plane', 'car', 'bird', 'cat',
+    #        'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # dataiter = iter(trainloader)
+    # images, labels = dataiter.next()
+
+    # imshow(torchvision.utils.make_grid(images))
+    # print(" ".join("%5s" % classes[labels[j]] for j in range(4)))
+    net = Net()
+    print(net)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(),lr=0.001, momentum=0.9)
+
+    for epoch in range(2):
+
+        running_loss = 0.0
+        for i,data in enumerate(trainloader,0):
+            inputs, labels = data
+            optimizer.zero_grad()
+
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' %
+                (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+    print('Finished Training')
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data,1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+    
