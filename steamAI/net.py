8dcedda8573f7d93146f57a0c3dc676cbbeb9e64
penglin <987532488@qq.com>
2019-06-03 20:37:32 +0800

    增加训练模型

--- steamAI/net.py
+++ steamAI/net.py
@@ -5,10 +5,11 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import data.dataset as dataset
 import torch.optim as optim
+import time
 # import pysnooper
 
-batch_size = 20
-epochs = 100
+batch_size = 5
+epochs = 1500
 batch_size_test = 1
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -16,19 +17,8 @@ class net(nn.Module):
     """网络"""
     def __init__(self):
         super(net,self).__init__()
-        self.fc = []
-        self.fc11 = nn.Linear(7,7)
-        self.fc.append(self.fc11)
-        self.fc12 = nn.Linear(7,7)
-        self.fc.append(self.fc12)
-        self.fc13 = nn.Linear(7,7)
-        self.fc.append(self.fc13)
-        self.fc14 = nn.Linear(7,7)
-        self.fc.append(self.fc14)
-        self.fc15 = nn.Linear(7,7)
-        self.fc.append(self.fc15)
-        self.fc16 = nn.Linear(3,3)
-        self.fc.append(self.fc16)
+        self.fc1 = nn.Linear(7,7)
+        self.fc2 = nn.Linear(3,3)
         self.MergeVertex = nn.Linear(38,50)
         self.dropout = nn.Dropout(p=0.96)
         self.fc3 = nn.Linear(50,30)
@@ -36,19 +26,11 @@ class net(nn.Module):
         self.fc5 = nn.Linear(20,1)
     def forward(self,x):
         """前向传播"""
-        x = x[0]
-        iput = []
-        oput = []
-        # print("x:",x)
-        for i in range(5):
-            iput.append(x[i*7:(i+1)*7])
-        iput.append(x[35:38])
-        # print("iput:",iput,len(iput))
-        for i,j in enumerate(iput):
-            # print("j:",j,j.shape)
-            a = self.fc[i](j)
-            oput.append(F.relu(a))
-        x = torch.cat([oput[0],oput[1],oput[2],oput[3],oput[4],oput[5]],dim=0)
+        output = []
+        for i in x.chunk(6,dim=1)[:-1]:
+            output.append(self.fc1(i))
+        output.append(self.fc2(x.chunk(6,dim=1)[-1]))
+        x = torch.cat(output,dim=1)
         # x = Variable(torch.tensor(x,dtype=torch.float))
         x = F.relu(self.MergeVertex(x))
         x = self.dropout(x)
@@ -117,24 +99,27 @@ def test(model,device,test_loader):
 
 
 if __name__ == "__main__":
-    # model = net2().to(DEVICE)
-    # optimizer = optim.Adam(model.parameters())
-    # datas = dataset.traindataset("./data/zhengqi_train.txt",train=True)
-    # trainloader = torch.utils.data.DataLoader(datas,batch_size=batch_size,shuffle=True,num_workers=0)
-    # lossfuc = nn.MSELoss()
-    # for epoch in range(1,epochs+1):
-    #     train(model,DEVICE,trainloader,optimizer,epoch,lossfuc)
-    # torch.save({"net":model.state_dict()},"./model_train{}_batch{}".format(epochs,batch_size))
+    model = net().to(DEVICE)
+    optimizer = optim.SGD(model.parameters(),lr=0.01)
+    datas = dataset.traindataset("./data/zhengqi_train.txt",train=True)
+    trainloader = torch.utils.data.DataLoader(datas,batch_size=batch_size,shuffle=True,num_workers=0)
+    lossfuc = nn.MSELoss()
+    for epoch in range(1,epochs+1):
+        train(model,DEVICE,trainloader,optimizer,epoch,lossfuc)
+    torch.save({"net":model.state_dict()},"./model_train{}".format(epochs))
 
-    model = net2().to(DEVICE)
-    obj = torch.load("./model_train100_batch10")
-    model.load_state_dict(obj['net'])
-    datas = dataset.traindataset("./data/zhengqi_test.txt",train=False)
-    testloader = torch.utils.data.DataLoader(datas,batch_size=batch_size_test,shuffle=False)
-    outputs = test(model,DEVICE,testloader)
-    with open("./data/result.txt","w") as file:
-        for output in outputs:
-            file.write(str(output.item())+"\n")
+
+    # model = net().to(DEVICE)
+    # obj = torch.load("./model_0603-18")
+    # model.load_state_dict(obj['net'])
+    # datas = dataset.traindataset("./data/zhengqi_test.txt",train=False)
+    # testloader = torch.utils.data.DataLoader(datas,batch_size=batch_size_test,shuffle=False)
+    # outputs = test(model,DEVICE,testloader)
+    # with open("./data/result.txt","w") as file:
+    #     for output in outputs:
+    #         file.write(str(output.item())+"\n")
+
+
     # traindatas,terget = datas[0] 
     # NET = net()
     # print(NET(traindatas))
