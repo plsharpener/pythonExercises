e86be8e620edb479adf4fb018e375811ee3eafea
ubuntu <ubuntu@localhost.localdomain>
2019-06-08 16:21:49 +0800

    xiu gai mo xing

--- steamAI/net.py
+++ steamAI/net.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import torch
 import numpy as np
 import torch.nn as nn
@@ -8,8 +9,8 @@ import torch.optim as optim
 import time
 # import pysnooper
 
-batch_size = 5
-epochs = 1500
+batch_size = 6
+epochs = 500
 batch_size_test = 1
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -20,7 +21,9 @@ class net(nn.Module):
         self.fc1 = nn.Linear(7,7)
         self.fc2 = nn.Linear(3,3)
         self.MergeVertex = nn.Linear(38,50)
-        self.dropout = nn.Dropout(p=0.96)
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc6 = nn.Linear(50,150)
+        self.fc7 = nn.Linear(150,50)
         self.fc3 = nn.Linear(50,30)
         self.fc4 = nn.Linear(30,20)
         self.fc5 = nn.Linear(20,1)
@@ -28,12 +31,15 @@ class net(nn.Module):
         """前向传播"""
         output = []
         for i in x.chunk(6,dim=1)[:-1]:
-            output.append(self.fc1(i))
-        output.append(self.fc2(x.chunk(6,dim=1)[-1]))
+            output.append(F.relu(self.fc1(i)))
+        output.append(F.relu(self.fc2(x.chunk(6,dim=1)[-1])))
         x = torch.cat(output,dim=1)
         # x = Variable(torch.tensor(x,dtype=torch.float))
         x = F.relu(self.MergeVertex(x))
         x = self.dropout(x)
+        x = F.relu(self.fc6(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc7(x))
         x = F.relu(self.fc3(x))
         x = F.relu(self.fc4(x))
         x = self.fc5(x)
@@ -45,7 +51,7 @@ class net2(nn.Module):
         self.fc1 = nn.Linear(38,38)
         # self.fc2 = nn.Linear(bitch_size*38,batch_size*38)
         self.fc2 = nn.Linear(38,50)
-        self.dropout = nn.Dropout(p=0.96)
+        self.dropout = nn.Dropout(p=0.5)
         self.fc3 = nn.Linear(50,30)
         self.fc4 = nn.Linear(30,20)
         self.fc5 = nn.Linear(20,1)
@@ -65,15 +71,9 @@ def train(model,device,train_loader,optimizer,epochs,lossfuc):
     """训练"""
     # model.train()
     for batch_idx,(data,target) in enumerate(train_loader):
-        # print("data:",data.shape) 
-        # print("target:",target)
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        # print(output)
-        # output = torch.unsqueeze(output,0)
-        # print(output)
-        # print("output.shape:{},target.shape:{}".format(output.shape,target.shape))
         loss = lossfuc(output,target)
         loss.backward()
         optimizer.step()
@@ -88,10 +88,7 @@ def test(model,device,test_loader):
     for batch_idx,data in enumerate(test_loader):
         data = data.to(device)
         output = model(data)
-        # outputs.append(list(output.numpy()))
         outputs.append(output)
-        # if batch_idx == 10:
-        #     break
     return outputs
 
     
@@ -99,25 +96,25 @@ def test(model,device,test_loader):
 
 
 if __name__ == "__main__":
+    #model = net().to(DEVICE)
+    #optimizer = optim.SGD(model.parameters(),lr=0.05)
+    #datas = dataset.traindataset("./data/zhengqi_train.txt",train=True)
+    #trainloader = torch.utils.data.DataLoader(datas,batch_size=batch_size,shuffle=True,num_workers=0)
+    #lossfuc = nn.MSELoss()
+    #for epoch in range(1,epochs+1):
+    #    train(model,DEVICE,trainloader,optimizer,epoch,lossfuc)
+    #torch.save({"net":model.state_dict()},"./model_train{}".format(epochs))
+
+
     model = net().to(DEVICE)
-    optimizer = optim.SGD(model.parameters(),lr=0.01)
-    datas = dataset.traindataset("./data/zhengqi_train.txt",train=True)
-    trainloader = torch.utils.data.DataLoader(datas,batch_size=batch_size,shuffle=True,num_workers=0)
-    lossfuc = nn.MSELoss()
-    for epoch in range(1,epochs+1):
-        train(model,DEVICE,trainloader,optimizer,epoch,lossfuc)
-    torch.save({"net":model.state_dict()},"./model_train{}".format(epochs))
-
-
-    # model = net().to(DEVICE)
-    # obj = torch.load("./model_0603-18")
-    # model.load_state_dict(obj['net'])
-    # datas = dataset.traindataset("./data/zhengqi_test.txt",train=False)
-    # testloader = torch.utils.data.DataLoader(datas,batch_size=batch_size_test,shuffle=False)
-    # outputs = test(model,DEVICE,testloader)
-    # with open("./data/result.txt","w") as file:
-    #     for output in outputs:
-    #         file.write(str(output.item())+"\n")
+    obj = torch.load("./model_train500")
+    model.load_state_dict(obj['net'])
+    datas = dataset.traindataset("./data/zhengqi_test.txt",train=False)
+    testloader = torch.utils.data.DataLoader(datas,batch_size=batch_size_test,shuffle=False)
+    outputs = test(model,DEVICE,testloader)
+    with open("./data/result.txt","w") as file:
+        for output in outputs:
+            file.write(str(output.item())+"\n")
 
 
     # traindatas,terget = datas[0] 
